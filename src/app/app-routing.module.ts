50409acfe93b0291256edd66f733c32aeb53d722
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-05-05 22:27:28 +0000

    Linked components to buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { Section1Component } from './section1/section1.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'section1', component: Section1Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Section1Component } from './section1/section1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Section1Component
   ],
   imports: [
     BrowserModule,
